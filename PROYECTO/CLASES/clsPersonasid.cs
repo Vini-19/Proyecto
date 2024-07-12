cbc8a67e78562327f4020a0bb9611d530e0b0dfd
MarcVin-hub <marcosanchez-1@outlook.com>
2024-07-12 01:31:36 -0600

    subir todo

---
+++ PROYECTO/CLASES/clsPersonasid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO.CLASES
+{
+    public class clsPersonasid
+    {
+        static string cod;
+
+        public string getcodUsu() { return cod; }
+        public void setcodUsu(string value) { cod = value; }
+
+
+    }
+}
---
+++ PROYECTO/iniciosesion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROYECTO.FORMULARIOS
+{
+    public partial class iniciosesion : Form
+    {
+        public iniciosesion()
+        {
+            InitializeComponent();
+        }
+
+        private void iniciosesion_Load(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
