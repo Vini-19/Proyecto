c59b9655075e07882f665cdfb086d8a038a7367e
MarcVin-hub <marcosanchez-1@outlook.com>
2024-07-11 23:13:51 -0600

    subir

--- frmAdmin/frmAdmin/CLASS/Conexion.cs
+++ frmAdmin/frmAdmin/CLASS/Conexion.cs
@@ -14,33 +14,32 @@ namespace frmAdmin.CLASS
     {
         public SqlConnection leer()
         {
-            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Proyecto_de_desarrolo.Properties.Settings.SQL"].ConnectionString);
-            
-            try
+            SqlConnection cn = new SqlConnection("Data Source=croas.database.windows.net;Initial Catalog=Proyecto_Carniceria;Persist Security Info=True;User ID=Vini;Password=Cr0as_P4ss;TrustServerCertificate=True");
+            if (cn.State == ConnectionState.Open)
             {
-                if (cn.State == ConnectionState.Closed)
-                {
-                    cn.Open();
-                }
+                cn.Close();
             }
-            catch (SqlException ex)
+            else
             {
-                MessageBox.Show("Error al intentar conectar: " + ex.Message);
+                cn.Open();
             }
-
             return cn;
+
         }
 
         public void RegistroUsu(string contra, string pregunta, string respuesta, string nomUsuario)
         {
+
+            Cconexion conexion = new Cconexion();
+
             try
             {
-                using (SqlConnection cn = leer())
+                SqlConnection cn = conexion.leer();
+                if (cn.State == ConnectionState.Open)
                 {
-                    SqlCommand comandoExisteUsuario = new SqlCommand("PA_RegistroUsuario", cn)
-                    {
-                        CommandType = CommandType.StoredProcedure
-                    };
+                    SqlCommand comandoExisteUsuario = new SqlCommand("PA_RegistroUsuario", cn);
+                    comandoExisteUsuario.CommandType = CommandType.StoredProcedure;
+
 
                     comandoExisteUsuario.Parameters.AddWithValue("@Contraseña", contra);
                     comandoExisteUsuario.Parameters.AddWithValue("@Roles_ID", 3);
@@ -48,5 +47,103 @@ namespace frmAdmin.CLASS
                     comandoExisteUsuario.Parameters.AddWithValue("@Respuesta_Recuperacion", respuesta);
                     comandoExisteUsuario.Parameters.AddWithValue("@Nombre_Usuario", nomUsuario);
 
+
                     comandoExisteUsuario.ExecuteNonQuery();
-                    MessageBox.Show("Usuario regi
+
+                    MessageBox.Show("Usuario registrado correctamente");
+                }
+                else
+                {
+                    MessageBox.Show("Error de conexión.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al intentar conectar: " + ex.Message);
+            }
+        }
+
+
+        public bool VerificarClienteExistente(string nombre)
+        {
+            Cconexion conexion = new Cconexion();
+            bool verificar = true;
+            try
+            {
+                using (SqlConnection cn = conexion.leer())
+                {
+                    if (cn.State == ConnectionState.Closed)
+                    {
+                        cn.Open();
+                    }
+
+                    SqlCommand comandoExisteCliente = new SqlCommand("PA_VerificarClienteExistente", cn);
+                    comandoExisteCliente.CommandType = CommandType.StoredProcedure;
+                    comandoExisteCliente.Parameters.AddWithValue("@nombre", nombre);
+
+                    int count = (int)comandoExisteCliente.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+
+                        verificar = true;
+
+                    }
+                    else
+                    {
+                        verificar = false;
+
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al intentar conectar: " + ex.Message);
+            }
+            return verificar;
+        }
+
+
+        public void RegistroCliente(string rtn, string dni, string nombre, int rol, bool estado, int telefono, string correo, string direccion, DateTime fecha)
+        {
+
+            Cconexion conexion = new Cconexion();
+
+            try
+            {
+                using (SqlConnection cn = conexion.leer())
+                {
+                    if (cn.State == ConnectionState.Closed)
+                    {
+                        cn.Open();
+                    }
+
+                    SqlCommand comandoInsertarCliente = new SqlCommand("PA_RegistrarCliente", cn);
+                    comandoInsertarCliente.CommandType = CommandType.StoredProcedure;
+                    comandoInsertarCliente.Parameters.AddWithValue("@RTN", rtn);
+                    comandoInsertarCliente.Parameters.AddWithValue("@DNI", dni);
+                    comandoInsertarCliente.Parameters.AddWithValue("@nombre", nombre);
+                    comandoInsertarCliente.Parameters.AddWithValue("@rol", rol);
+                    comandoInsertarCliente.Parameters.AddWithValue("@estado", estado);
+                    comandoInsertarCliente.Parameters.AddWithValue("@telefono", telefono);
+                    comandoInsertarCliente.Parameters.AddWithValue("@correo", correo);
+                    comandoInsertarCliente.Parameters.AddWithValue("@direccion", direccion);
+                    comandoInsertarCliente.Parameters.AddWithValue("@fecha_Inscripcion", fecha);
+
+                    comandoInsertarCliente.ExecuteNonQuery();
+
+                    MessageBox.Show("Cliente registrado correctamente");
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al intentar conectar: " + ex.Message);
+            }
+        }
+    }
+}
